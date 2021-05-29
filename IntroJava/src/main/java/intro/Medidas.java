f0fc889de6e182f5aeb4162fab1518fab3bc966e
Roberta Aguilar <roberta.aguilar@kobel.com.br>
2021-05-29 13:20:31 -0300

    intro java

---
+++ IntroJava/src/main/java/intro/Medidas.java
@@ -0,0 +1,12 @@
+// 1 - Pacote
+package intro;
+
+// 2 - Referencia as bibliotecas
+
+// 3 - Classe
+public class Medidas {
+    // 3.1 - Atributos - Caracteristicas
+
+    // 3.2 - Metodos e funcoes
+
+}
