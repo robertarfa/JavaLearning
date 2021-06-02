12b3b611304a42134c2f11caaa246f4947982c66
Roberta Aguilar <roberta.aguilar@kobel.com.br>
2021-06-02 09:25:04 -0300

    exercicio 1 - add and remove

---
+++ basicDio/src/main/java/aulas/exercicio1/Carro.java
@@ -0,0 +1,18 @@
+package aulas.exercicio1;
+
+public class Carro {
+    public int numeroPessoasNoCarro;
+
+  public int getNumeroPessoasNoCarro(){
+      return numeroPessoasNoCarro;
+  }
+
+  public int addPessoasNoCarro(){
+      return numeroPessoasNoCarro =+ 1;
+  }
+
+    public int removePessoasNoCarro(){
+        return numeroPessoasNoCarro = numeroPessoasNoCarro - 1;
+    }
+
+}
---
+++ basicDio/src/main/java/aulas/exercicio1/metodoCarro.java
@@ -0,0 +1,14 @@
+package aulas.exercicio1;
+
+public class metodoCarro {
+
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+
+        System.out.println(carro.getNumeroPessoasNoCarro());
+        System.out.println(carro.addPessoasNoCarro());
+        System.out.println("Quantas pessoas tem no carro? " + carro.numeroPessoasNoCarro);
+        System.out.println(carro.removePessoasNoCarro());
+        System.out.println("Quantas pessoas tem no carro? " + carro.numeroPessoasNoCarro);
+    }
+}
